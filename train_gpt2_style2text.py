c793388fa603074db75b25f08a2919cce58f6152
Dokhyam <dokhyam.hoshen@mail.huji.ac.il>
2021-05-06 19:41:45 +0300

    Update train_gpt2_style2text.py

--- train_gpt2_style2text.py
+++ train_gpt2_style2text.py
@@ -1,4 +1,3 @@
-# pip install datasets
 import os
 import datasets
 from transformers import AdamW, get_linear_schedule_with_warmup
@@ -10,23 +9,23 @@ from Models.GPT2 import GPT2
 from Configs.train_config import config
 
 def tokenize_function(examples):
-    print(examples)
-    return tokenizer(examples["text"])
+	print(examples)
+	return tokenizer(examples["text"])
 
 def group_texts(examples):
-    # Concatenate all texts.
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-    total_length = (total_length // block_size) * block_size
-    # Split by chunks of max_len.
-    result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    result["labels"] = result["input_ids"].copy()
-    return result
+	# Concatenate all texts.
+	concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+	total_length = len(concatenated_examples[list(examples.keys())[0]])
+	# We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+		# customize this part to your needs.
+	total_length = (total_length // block_size) * block_size
+	# Split by chunks of max_len.
+	result = {
+		k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+		for k, t in concatenated_examples.items()
+	}
+	result["labels"] = result["input_ids"].copy()
+	return result
 
 BASE_PATH = '/disk1/dokhyam/Style2Text/'
 d_data_path = BASE_PATH + 'directions/'
@@ -35,7 +34,7 @@ val_sentences_data_path = BASE_PATH + 'sentences.txt'
 
 saved_models_path = '/home/dokhyam/Models/'
 if not os.path.exists(saved_models_path):
-    os.mkdir(saved_models_path)
+	os.mkdir(saved_models_path)
 # Training and optimization configs 
 gpt2 = GPT2()
 model = gpt2.model.train()
@@ -48,15 +47,15 @@ from datasets import load_dataset
 datasets = load_dataset("text", data_files={"train":sentences_data_path , "validation": val_sentences_data_path})
 tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
 lm_datasets = tokenized_datasets.map(
-    group_texts,
-    batched=True,
-    batch_size=64,
-    num_proc=4,
+	group_texts,
+	batched=True,
+	batch_size=64,
+	num_proc=4,
 )
 
 optimizer = AdamW(model.parameters(), lr= config['lr'])
 scheduler = get_linear_schedule_with_warmup(
-    optimizer, num_warmup_steps=5000, num_training_steps=-1
+	optimizer, num_warmup_steps=5000, num_training_steps=-1
 )
 
 loss = 0
@@ -66,30 +65,29 @@ batch_size=64
 
 # Train/Validation loops
 for epoch in range(epochs):
-    with tqdm(total=len(lm_datasets['train']) / 2) as pbar:
-        for idx,entry in enumerate(lm_datasets['train']):
+	with tqdm(total=len(lm_datasets['train']) / 2) as pbar:
+		for idx,entry in enumerate(lm_datasets['train']):
 
-            if idx % 2000 == 0 and idx != 0:
-              for i in range(batch_size):
-                with torch.no_grad():
-                    outputs = model.generate(validation_input_encodings[i], num_beams=2, no_repeat_ngram_size=2, max_length=max_length+1, pad_token_id=pad_token_id)
-                    output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-            
-            if idx % 50000 == 0:
-              torch.save(model.state_dict(), f'{saved_models_path}{idx}_{time.time()}_{int(loss)}.bin')
+			if idx % 2000 == 0 and idx != 0:
+			  for i in range(batch_size):
+				with torch.no_grad():
+					outputs = model.generate(validation_input_encodings[i], num_beams=2, no_repeat_ngram_size=2, max_length=max_length+1, pad_token_id=pad_token_id)
+					output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+			
+			if idx % 50000 == 0:
+			  torch.save(model.state_dict(), f'{saved_models_path}{idx}_{time.time()}_{int(loss)}.bin')
 
-            model.zero_grad()
+			model.zero_grad()
 
-            inputs = torch.LongTensor(entry['input_ids']).cuda()
-            attn_masks = torch.FloatTensor(entry['attention_mask']).cuda()
-            labels = torch.LongTensor(entry['labels']).cuda()
-            outputs = model(inputs, labels=labels, attention_mask = attn_masks)
+			inputs = torch.LongTensor(entry['input_ids']).cuda()
+			attn_masks = torch.FloatTensor(entry['attention_mask']).cuda()
+			labels = torch.LongTensor(entry['labels']).cuda()
+			outputs = model(inputs, labels=labels, attention_mask = attn_masks)
 
-            loss = outputs['loss']
-            if idx % 300:
-                print('loss: ' + str(loss.detach()))
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
+			loss = outputs['loss']
+			loss.backward()
+			optimizer.step()
+			scheduler.step()
 
-            pbar.update(2)
+			pbar.update(2)
+	print('loss: ' + str(loss.detach()))
