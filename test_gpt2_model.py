1214aa18f146f9695bd7d6f373d62dd6257229ed
Dokhyam <dokhyam.hoshen@mail.huji.ac.il>
2021-05-09 17:13:29 +0300

    Update test_gpt2_model.py

--- test_gpt2_model.py
+++ test_gpt2_model.py
@@ -6,8 +6,28 @@ import time
 from Models.GPT2 import GPT2
 from Configs.train_config import config
 
+def tokenize_function(examples):
+	print(examples)
+	return tokenizer(examples["text"])
+
+def group_texts(examples):
+	# Concatenate all texts.
+	concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+	total_length = len(concatenated_examples[list(examples.keys())[0]])
+	# We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+		# customize this part to your needs.
+	total_length = (total_length // block_size) * block_size
+	# Split by chunks of max_len.
+	result = {
+		k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+		for k, t in concatenated_examples.items()
+	}
+	result["labels"] = result["input_ids"].copy()
+	return result
+
 BASE_PATH = '/disk1/dokhyam/Style2Text/'
 path_model = BASE_PATH +  '/Refined_gpt_models/Epoch_19_iteration_50.pt'
+path_prefixes = BASE_PATH + 'prefix_examples.txt'
 gpt2 = GPT2()
 gpt2.load_model(path_model)
 gpt2.model = gpt2.model.eval().cuda()
@@ -15,23 +35,21 @@ tested_model = gpt2
 max = 20
 eof = '<|endoftext|>'
 pad_token_id = tested_model.tokenizer('[PAD]')['input_ids'][0]
+
+from datasets import load_dataset
+datasets = load_dataset("text", data_files={"train":sentences_data_path , "test": path_prefixes})
+tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
+lm_datasets = tokenized_datasets.map(
+	group_texts,
+	batched=True,
+	batch_size=2,
+	num_proc=4,
+)
 # Test
-
-textual_data = tested_model.tokenizer.decode(token_ids = '', skip_special_tokens=False).split('<comment>')
-target_text = textual_data[1].split(eof)[0]
-# targets.append(target_text)
-input_text = textual_data[0] 
-# inputs.append(input_text)
-
-comment_idx = list(proccessed_data[i]).index(dataset.comment_encoding) + 1
-input_encoding = proccessed_data[i][:comment_idx].unsqueeze(0).cuda()
-
-input_encodings.append(input_encoding)
-
-results = []
-with torch.no_grad():
-    outputs = tested_model.model.generate(input_encodings, num_beams=2, no_repeat_ngram_size=2, max_length=max+1, pad_token_id=pad_token_id)
-    output_text = tested_model.tokenizer.decode(outputs[0], skip_special_tokens=True)
-    results.append(output_text)
+for idx,entry in enumerate(lm_datasets['test'])
+	inputs = torch.LongTensor(entry['input_ids']).cuda()
+	outputs = tested_model.generate(inputs[0], num_beams=2, no_repeat_ngram_size=2, max_length=max+1, pad_token_id=pad_token_id)
+	textual_data = tested_model.tokenizer.decode(outputs[0], skip_special_tokens=True)
+	print(textual_data)
 
 
